5f7e80a557f033683ada424bdf69afadff5e9ec6
VatsalSoni301 <sonivatsal111@gmail.com>
2018-08-29 15:46:01 +0530

    snapshot implemented successfully

--- snapshot.cpp
+++ snapshot.cpp
@@ -1,27 +1,30 @@
 #include "config.h"
 
-vector<string> findf;
-
+//vector<string> findf;
+fstream newfile;
+char *create_argument;
 int snapShot(vector<string> &commandSplit)
 {
-	char *create_argument= new char[commandSplit[2].length()+1];
+	create_argument= new char[commandSplit[2].length()+1];
 	strcpy(create_argument,commandSplit[2].c_str());
 	open(create_argument, O_WRONLY|O_CREAT, S_IRUSR|S_IWUSR|S_IXUSR|S_IRGRP|S_IXGRP|S_IXOTH);
-	findf.push_back(commandSplit[1]);
+	//findf.push_back(commandSplit[1]);
+	newfile.close();
 	findall(commandSplit[1]);
 	
-    ofstream newfile;
-  	newfile.open(create_argument);
-  	for(unsigned i=0;i<findf.size();i++)
+  	/*for(unsigned i=0;i<findf.size();i++)
   	{
   		newfile<<findf[i]<<endl;
-  	}
-  	newfile.close();
+  	}*/
 	return 0;
 }
 
 void findall(string rm)
 {
+	//create_argument= new char[rm.length()+1];
+	//strcpy(create_argument,rm.c_str());
+	newfile.open(create_argument,fstream::app);
+	
 	struct dirent **namelist;
 	int i;
 	struct stat statObj;
@@ -30,7 +33,17 @@ void findall(string rm)
 	strcpy(rm1,rm.c_str());
 	char *rm2;
 	int total = scandir(rm1,&namelist, NULL,alphasort);
-
+	if(total>2)
+		newfile<<rm<<endl<<endl;
+	for(i=0;i<total;i++)
+	{
+		if(string(namelist[i]->d_name)!="." && string(namelist[i]->d_name)!="..")
+		{
+			newfile<<namelist[i]->d_name<<"      ";
+		}
+		//newfile<<" ";
+	}
+	newfile<<endl<<endl;
 	for(i=0;i<total;i++)
 	{
 		path=rm+"/"+namelist[i]->d_name;
@@ -47,13 +60,15 @@ void findall(string rm)
 			//int f=current.find_last_of("/\\");
 	    	//current=current.substr(0,f);
 			//path=rm+"/"+namelist[i]->d_name;
-			findf.push_back(path);
+			
+			//findf.push_back(path);
+			newfile.close();
 			findall(path);
 		}
 		else
 		{
-			if(string(namelist[i]->d_name)!="." && string(namelist[i]->d_name)!="..")
-				findf.push_back(path);
+			//if(string(namelist[i]->d_name)!="." && string(namelist[i]->d_name)!="..")
+				//newfile<<namelist[i]->d_name<<endl;
 		}
 	}
 }
